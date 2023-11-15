bfdd5efc9d9ee84a93a372c9e6f795a008475976
Madd-G <akhmadnuralamsyah@gmail.com>
2023-11-15 16:18:59 +0700

    feat: Integrate project with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:smatrackz/core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const Scaffold(),
     );
   }
 }
