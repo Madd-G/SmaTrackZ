9f06c316c5e38dda2b58bb51adf8c9eb9a735d08
Madd-G <akhmadnuralamsyah@gmail.com>
2023-11-25 08:33:59 +0700

    fix: Resolve issue with profile data not updating on database changes

--- lib/core/common/widgets/profile_card.dart
+++ lib/core/common/widgets/profile_card.dart
@@ -7,9 +7,7 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = context.currentUser!;
     return Container(
-      // height: 120.0,
       width: context.width,
       decoration: const BoxDecoration(
         color: Colors.white,
@@ -19,66 +17,61 @@ class ProfileCard extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Consumer<UserProvider>(
-              builder: (_, provider, __) {
-                final user = provider.user!;
-                final image =
-                    user.profilePic == null || user.profilePic!.isEmpty
-                        ? null
-                        : user.profilePic;
-                return CircleAvatar(
+        child: Consumer<UserProvider>(
+          builder: (_, provider, __) {
+            final user = provider.user!;
+            final image = user.profilePic == null || user.profilePic!.isEmpty
+                ? null
+                : user.profilePic;
+            return Row(
+              children: [
+                CircleAvatar(
                   radius: 50,
                   backgroundImage: image != null
                       ? NetworkImage(image)
                       : const AssetImage(AppMedia.user) as ImageProvider,
-                );
-              },
-            ),
-            const SizedBox(
-              width: 10.0,
-            ),
-            Flexible(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    user.fullName,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w700, fontSize: 17.0),
-                  ),
-                  const SizedBox(
-                    height: 5.0,
-                  ),
-                  const RoundedContainer(
-                    containerColor: AppColors.secondaryColor,
-                    borderColor: AppColors.secondaryColor,
-                    radius: 10.0,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-                      child: Text(
-                        'Mobile Developer',
-                        // 'Head Human Resource Development',
-                        // 'Data Analytics',
-                        style: TextStyle(
-                          color: AppColors.primaryColor,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15.0,
-                        ),
-                        maxLines: 2,
+                ),
+                const SizedBox(width: 10.0),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user.fullName,
                         overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 17.0),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                      const SizedBox(height: 5.0),
+                      const RoundedContainer(
+                        containerColor: AppColors.secondaryColor,
+                        borderColor: AppColors.secondaryColor,
+                        radius: 10.0,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 12.0),
+                          child: Text(
+                            'Mobile Developer',
+                            // 'Head Human Resource Development',
+                            // 'Data Analytics',
+                            style: TextStyle(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 15.0,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
     );
