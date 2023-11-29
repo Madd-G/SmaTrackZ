11993778fa5e9026f61111777ae6bf215b508307
Madd-G <akhmadnuralamsyah@gmail.com>
2023-11-29 16:43:23 +0700

    fix: Resolve bug preventing automatic update when location data changes

--- lib/src/modules/hrd/office/presentation/views/office_screen.dart
+++ lib/src/modules/hrd/office/presentation/views/office_screen.dart
@@ -13,91 +13,71 @@ class OfficeScreen extends StatelessWidget {
         extendBodyBehindAppBar: true,
         appBar: const OfficeAppBar(),
         body: BackgroundImage(
-          child: BlocBuilder<OfficeBloc, OfficeState>(
-            builder: (context, state) {
-              if (state is OfficeInitialState) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is OfficeLoadedState) {
-                final office = OfficeModel(
-                  officeId: state.officeData.officeId,
-                  officeName: state.officeData.officeName,
-                  address: state.officeData.address,
-                  latitude: state.officeData.latitude,
-                  longitude: state.officeData.longitude,
-                  website: state.officeData.website,
-                );
-                context.officeProvider.initOffice(office);
-                return SingleChildScrollView(
-                  child: SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16.0,
-                        right: 16.0,
-                        bottom: 24.0,
-                      ),
-                      child: Consumer<OfficeProvider>(
-                        builder: (_, provider, __) {
-                          final office = provider.office!;
-                          return RoundedContainer(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Expanded(
-                                        child: Text(office.officeName,
-                                            maxLines: 2,
-                                            style: CustomTextStyle
-                                                .headingSemiBold),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 20.0),
-                                  OfficeInfo(
-                                    icon: Icons.location_on,
-                                    info: office.address,
-                                  ),
-                                  const SizedBox(height: 20.0),
-                                  const OfficeInfo(
-                                    icon: Icons.access_time,
-                                    info: '09.00 - 18.00',
-                                  ),
-                                  const SizedBox(height: 20.0),
-                                  const OfficeInfo(
-                                    icon: Icons.phone,
-                                    info: '(0281) 344597',
-                                  ),
-                                  const SizedBox(height: 20.0),
-                                  const OfficeInfo(
-                                    icon: Icons.person,
-                                    info: '581 people',
-                                  ),
-                                  LocationPicker(
-                                    id: 'officeLocation',
-                                    buttonLabel: 'View Location',
-                                    latitude: office.latitude,
-                                    longitude: office.longitude,
-                                    onChanged: (latitude, longitude) {
-                                      latitude = latitude;
-                                      longitude = longitude;
-                                    },
-                                  )
-                                ],
-                              ),
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  bottom: 24.0,
+                ),
+                child: Consumer<OfficeProvider>(
+                  builder: (_, provider, __) {
+                    final office = provider.office!;
+                    return RoundedContainer(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(office.officeName,
+                                      maxLines: 2,
+                                      style: CustomTextStyle
+                                          .headingSemiBold),
+                                ),
+                              ],
                             ),
-                          );
-                        },
+                            const SizedBox(height: 20.0),
+                            OfficeInfo(
+                              icon: Icons.location_on,
+                              info: office.address,
+                            ),
+                            const SizedBox(height: 20.0),
+                            const OfficeInfo(
+                              icon: Icons.access_time,
+                              info: '09.00 - 18.00',
+                            ),
+                            const SizedBox(height: 20.0),
+                            const OfficeInfo(
+                              icon: Icons.phone,
+                              info: '(0281) 344597',
+                            ),
+                            const SizedBox(height: 20.0),
+                            const OfficeInfo(
+                              icon: Icons.person,
+                              info: '581 people',
+                            ),
+                            LocationPicker(
+                              id: 'officeLocation',
+                              buttonLabel: 'View Location',
+                              latitude: office.latitude,
+                              longitude: office.longitude,
+                              onChanged: (latitude, longitude) {
+                                latitude = latitude;
+                                longitude = longitude;
+                              },
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              } else if (state is OfficeErrorState) {
-                return Center(child: Text(state.errorMessage));
-              }
-              return const SizedBox();
-            },
-          ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          )
         ),
       ),
     );
